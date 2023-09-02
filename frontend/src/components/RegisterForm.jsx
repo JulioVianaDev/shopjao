abd15dfbfe4c4826bcedcc62a6282d1f089b4934
JulioVianaDev <JulioVianaDev@gmail.com>
2023-09-02 09:58:19 -0300

    proximos passos é implementar a função

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,8 +1,15 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import "./Form.css"
 import "./RegisterForm.css"
 function RegisterForm({changeState}) {
-  
+  const [role,setRole] =useState(null)
+  const [name,setName] =useState("")
+  const [idade,setIdade] =useState(0)
+  const [email,setEmail] =useState("")
+  const [password,setPassword] =useState("")
+  const [confirmPassword,setConfirmPassword] =useState("")
+  const [image,setImage] =useState(null)
+
   useEffect(()=>{
     const fileInput = document.getElementById("image");
     const imageLabel = document.getElementById("imageLabel");
@@ -24,11 +31,29 @@ function RegisterForm({changeState}) {
   return (
       <form className="form">
         <label htmlFor="name">Nome</label>
-        <input type="text" id="name" name="name" />
+        <input 
+          type="text" 
+          id="name" 
+          name="name" 
+          onChange={(e)=>setName(e.target.value)}
+          value={name}
+        />
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" name="email" />
+        <input 
+          type="text" 
+          id="email" 
+          name="email" 
+          onChange={(e)=>setEmail(e.target.value)}
+          value={email}
+        />
         <label htmlFor="idade">Idade</label>
-        <input type="number" id="idade" name="idade" />
+        <input 
+          type="number" 
+          id="idade" 
+          name="idade" 
+          onChange={(e)=>setIdade(e.target.value)}
+          value={idade}
+        />
         <label id="imageLabel" htmlFor="image">Envie sua imagem</label>
         <input 
           type="file" 
@@ -36,12 +61,23 @@ function RegisterForm({changeState}) {
           name="image" 
           required
           accept="image/*"
-
         />
         <label htmlFor="password">Senha</label>
-        <input type="text" id="password" name="password" />
+        <input 
+          type="text" 
+          id="password" 
+          name="password" 
+          onChange={(e)=>setPassword(e.target.value)}
+          value={password}
+        />
         <label  htmlFor="confirmPassword">Confirme sua senha</label>
-        <input type="text" id="confirmPassword" name="confirmPassword" />
+        <input 
+          type="text" 
+          id="confirmPassword" 
+          name="confirmPassword" 
+          onChange={(e)=>setConfirmPassword(e.target.value)}
+          value={confirmPassword}
+        />
         <button type="submit">Criar conta</button>
         <div className="" onClick={changeState}>Login</div>
       </form>
