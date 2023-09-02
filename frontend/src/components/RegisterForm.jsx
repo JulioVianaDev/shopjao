27aee1cff1025c3f6f0ee9ebeed200a3fc4cc559
JulioVianaDev <JulioVianaDev@gmail.com>
2023-09-02 09:36:32 -0300

    reader js vanilla

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import "./Form.css"
+import "./RegisterForm.css"
 function RegisterForm({changeState}) {
+  
+  useEffect(()=>{
+    const fileInput = document.getElementById("image");
+    const imageLabel = document.getElementById("imageLabel");
+
+    fileInput.addEventListener("change",function(){
+      if(fileInput.files.length > 0 && fileInput.files[0].type.startsWith("image/")){
+        const reader = new FileReader();
+        reader.onload = function(event){
+          imageLabel.innerHTML= "";
+          imageLabel.style.backgroundImage = `url(${event.target.result})`;
+        }
+        reader.readAsDataURL(fileInput.files[0])
+      }else{
+        imageLabel.style.backgroundImage = "";
+        imageLabel.innerHTML = "Envie sua imagem"
+      }
+    })
+  },[])
   return (
       <form className="form">
         <label htmlFor="name">Nome</label>
@@ -9,11 +29,18 @@ function RegisterForm({changeState}) {
         <input type="text" id="email" name="email" />
         <label htmlFor="idade">Idade</label>
         <input type="number" id="idade" name="idade" />
-        <label htmlFor="image">Image</label>
-        <input type="file" id="image" name="image" />
+        <label id="imageLabel" htmlFor="image">Envie sua imagem</label>
+        <input 
+          type="file" 
+          id="image" 
+          name="image" 
+          required
+          accept="image/*"
+
+        />
         <label htmlFor="password">Senha</label>
         <input type="text" id="password" name="password" />
-        <label htmlFor="confirmPassword">Confirme sua senha</label>
+        <label  htmlFor="confirmPassword">Confirme sua senha</label>
         <input type="text" id="confirmPassword" name="confirmPassword" />
         <button type="submit">Criar conta</button>
         <div className="" onClick={changeState}>Login</div>
