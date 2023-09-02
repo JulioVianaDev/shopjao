7493864c50c44306422e4f982ab485b83148262b
JulioVianaDev <JulioVianaDev@gmail.com>
2023-09-02 09:09:52 -0300

    mudanças no input

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -7,6 +7,10 @@ function RegisterForm({changeState}) {
         <input type="text" id="name" name="name" />
         <label htmlFor="email">Email</label>
         <input type="text" id="email" name="email" />
+        <label htmlFor="idade">Idade</label>
+        <input type="number" id="idade" name="idade" />
+        <label htmlFor="image">Image</label>
+        <input type="file" id="image" name="image" />
         <label htmlFor="password">Senha</label>
         <input type="text" id="password" name="password" />
         <label htmlFor="confirmPassword">Confirme sua senha</label>
